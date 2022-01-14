a0e83447d93a604ea786e73e03c846b06644b552
SA.Snow <snowasnow@gmail.com>
2022-01-14 14:10:47 +0800

    Initial Commit

---
+++ FlacTest/FlacTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlacTestApp.swift
+//  FlacTest
+//
+//  Created by SA.Snow on 2022/1/14.
+//
+
+import SwiftUI
+
+@main
+struct FlacTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
