a43daae071514a1ec53850f8e493b02cc8211427
SA.Snow <snowasnow@gmail.com>
2022-01-14 14:20:27 +0800

    1

--- FlacTest/ContentView.swift
+++ FlacTest/ContentView.swift
@@ -6,11 +6,35 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    @State var audioPlayer: AVAudioPlayer?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack (spacing: 30) {
+            Button {
+                playAudio(forResource: "梁静茹-勇气", ofType: "flac")
+            } label: {
+                Text("play flac")
+            }
+            Button {
+                playAudio(forResource: "七里香", ofType: "mp3")
+            } label: {
+                Text("play mp3")
+            }
+        }
+    }
+    
+    func playAudio(forResource: String, ofType: String) {
+        let path = Bundle.main.path(forResource: forResource, ofType: ofType)!
+        let url = URL(fileURLWithPath: path)
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+        } catch {
+            print("音频文件出现问题")
+        }
     }
 }
 
